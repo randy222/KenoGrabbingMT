d7fb43a236001e5c04db56481481395bbfbd2c42
TW\pohsun <pohsun222@gmail.com>
2018-05-07 14:05:02 +0800

    no message

--- src/main/java/com/grabbing/task/KenoGrabbingMT.java
+++ src/main/java/com/grabbing/task/KenoGrabbingMT.java
@@ -49,76 +49,80 @@ public class KenoGrabbingMT extends TimerTask{
 	@Override
 	public void run() {		
 		
-		List<IPInfo> ipList = IPUtils.checkIP();		
-		Document doc = grap(cookies, ipList);
-		String cont = doc.toString();
-		int idx = cont.indexOf("recaptcha_response_field");
-
-		if (idx >= 0) {
-			logger.info("Authorize result: failed");
-			writeErrMsg("5");
-		} else {		
-			Elements drawNos = doc.select(".restext");
-			Elements numbers = doc.getElementsByAttributeValue("style", "color:#002160");
+		try {
+			List<IPInfo> ipList = IPUtils.checkIP();		
+			Document doc = grap(cookies, ipList);
+			String cont = doc.toString();
+			int idx = cont.indexOf("recaptcha_response_field");
 	
-			int numberOfDraws = 20;
-			int size = drawNos.size() / 2;
-			size = size <= numberOfDraws ? size : numberOfDraws;
-			List<String> drawNoList = new ArrayList<String>();
-			List<String> drawResultList = new ArrayList<String>();
-			for (Element no : drawNos) {
-				if (!no.text().contains(":")) {
-					drawNoList.add(no.text());
+			if (idx >= 0) {
+				logger.info("Authorize result: failed");
+				writeErrMsg("5");
+			} else {		
+				Elements drawNos = doc.select(".restext");
+				Elements numbers = doc.getElementsByAttributeValue("style", "color:#002160");
+		
+				int numberOfDraws = 20;
+				int size = drawNos.size() / 2;
+				size = size <= numberOfDraws ? size : numberOfDraws;
+				List<String> drawNoList = new ArrayList<String>();
+				List<String> drawResultList = new ArrayList<String>();
+				for (Element no : drawNos) {
+					if (!no.text().contains(":")) {
+						drawNoList.add(no.text());
+					}
 				}
+				for (Element number : numbers) {
+					drawResultList.add(number.text());
+				}			
+				
+				try {
+					logger.info("Start writing file");
+					PrintWriter writer = new PrintWriter("/usr/local/applications/kn-grabbing-server/Keno-Grabbing-MT.txt", "UTF-8");	
+	//				PrintWriter writer = new PrintWriter("C:\\Users\\pohsun\\Desktop\\Keno-Grabbing-MT.txt", "UTF-8");	
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+					writer.println("Message_Code: 0");
+					writer.println("Last updated time: " + sdf.format(Calendar.getInstance().getTime()));
+					writer.println("Version: "+count);
+					String line = "";
+					for (int i = 0; i < size; i++) {
+						line += drawNoList.get(i) + "@[";
+						line += drawResultList.get(0) + ",";
+						line += drawResultList.get(1) + ",";
+						line += drawResultList.get(2) + ",";
+						line += drawResultList.get(3) + ",";
+						line += drawResultList.get(4) + ",";
+						line += drawResultList.get(5) + ",";
+						line += drawResultList.get(6) + ",";
+						line += drawResultList.get(7) + ",";
+						line += drawResultList.get(8) + ",";
+						line += drawResultList.get(9) + ",";
+						line += drawResultList.get(10) + ",";
+						line += drawResultList.get(11) + ",";
+						line += drawResultList.get(12) + ",";
+						line += drawResultList.get(13) + ",";
+						line += drawResultList.get(14) + ",";
+						line += drawResultList.get(15) + ",";
+						line += drawResultList.get(16) + ",";
+						line += drawResultList.get(17) + ",";
+						line += drawResultList.get(18) + ",";
+						line += drawResultList.get(19) + "]";
+						if (drawResultList.size() >= 20) {
+							removeData(drawResultList);
+						}	
+						logger.info(line);
+						writer.println(line);
+						line = "";
+					}
+					writer.close();
+					logger.info("End writing file");
+				} catch (Exception e) {	
+					logger.error("Error in drawing MT data. Error message: ", e);		
+				} 
+				count++;	
 			}
-			for (Element number : numbers) {
-				drawResultList.add(number.text());
-			}			
-			
-			try {
-				logger.info("Start writing file");
-				PrintWriter writer = new PrintWriter("/usr/local/applications/kn-grabbing-server/Keno-Grabbing-MT.txt", "UTF-8");	
-//				PrintWriter writer = new PrintWriter("C:\\Users\\pohsun\\Desktop\\Keno-Grabbing-MT.txt", "UTF-8");	
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-				writer.println("Message_Code: 0");
-				writer.println("Last updated time: " + sdf.format(Calendar.getInstance().getTime()));
-				writer.println("Version: "+count);
-				String line = "";
-				for (int i = 0; i < size; i++) {
-					line += drawNoList.get(i) + "@[";
-					line += drawResultList.get(0) + ",";
-					line += drawResultList.get(1) + ",";
-					line += drawResultList.get(2) + ",";
-					line += drawResultList.get(3) + ",";
-					line += drawResultList.get(4) + ",";
-					line += drawResultList.get(5) + ",";
-					line += drawResultList.get(6) + ",";
-					line += drawResultList.get(7) + ",";
-					line += drawResultList.get(8) + ",";
-					line += drawResultList.get(9) + ",";
-					line += drawResultList.get(10) + ",";
-					line += drawResultList.get(11) + ",";
-					line += drawResultList.get(12) + ",";
-					line += drawResultList.get(13) + ",";
-					line += drawResultList.get(14) + ",";
-					line += drawResultList.get(15) + ",";
-					line += drawResultList.get(16) + ",";
-					line += drawResultList.get(17) + ",";
-					line += drawResultList.get(18) + ",";
-					line += drawResultList.get(19) + "]";
-					if (drawResultList.size() >= 20) {
-						removeData(drawResultList);
-					}	
-					logger.info(line);
-					writer.println(line);
-					line = "";
-				}
-				writer.close();
-				logger.info("End writing file");
-			} catch (Exception e) {	
-				logger.error("Error in drawing MT data. Error message: ", e);		
-			} 
-			count++;	
+		} catch (Exception e) {
+			logger.error("Error in drawing MT data. Error message: ", e);
 		}
 	}
 	
